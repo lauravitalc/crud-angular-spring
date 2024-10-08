ef3e7f738f2e1b32733afcd018207e54a684338d
lauravitalc <laura_vitalc@hotmail.com>
2024-10-07 22:01:29 -0300

    Class 9 - Chamada HTTP Get no Angular e RXJS

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,16 +10,10 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent implements OnInit {
-
-  courses:Course[] = [];
+  courses: Observable<Course[]>; // We need to put this as Observable, because we are using HttpClient, that returns an Observable
   displayedColumns = ['name', 'category'];
 
-  //coursesService: CoursesService;
-
   constructor(private courseService: CoursesService) {
-    //this.courses = [] -> If we initialize "courses:Course[];" only, we need to put this
-    // this.coursesService = new CoursesService();
-    //this.courses = this.coursesService.list();
     this.courses = courseService.list(); // We can use this way, because we put the service in the constructor. We can put this on ngInit as well if you think it's better.
   }
 
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { Observable, tap, first } from 'rxjs';
 
 @Injectable({
   providedIn: 'root' // Significa que estará disponível para toda a aplicação
 })
 export class CoursesService {
-
+  private readonly API = '/assets/courses.json'; // Caminho do meu endpoint
   constructor(private httpClient: HttpClient) { } // Precisaremos importar o httpClient também para o app.module.ts, para ficar "global"
 
-  list(): Course[] {
-    return [
-      {_id: '1', name: 'Angular', 'category': 'Front-End'},
-      {_id: '2', name: 'Java', 'category': 'Back-End'}
-    ];
+  list(): Observable<any> {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      //take(1), // O take(1) faz com que o Observable seja finalizado após a primeira emissão
+      first(), // O first() faz com que o Observable seja finalizado após a primeira emissão
+      tap(courses => console.log(courses)) // O tap é um operador que permite executar um efeito colateral em um Observable sem afetar o valor do mesmo
+    ); // Retorna um Observable
   }
 }
+
