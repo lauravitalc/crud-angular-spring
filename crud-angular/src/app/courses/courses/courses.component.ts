9a61db083a11a7e4f9603b213ec43f114b85890e
lauravitalc <laura_vitalc@hotmail.com>
2024-11-07 21:13:50 -0300

    11 - Lista de Cursos: Tratamento de Erros e MatDialog

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -13,8 +15,23 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>; // We need to put this as Observable, because we are using HttpClient, that returns an Observable
   displayedColumns = ['name', 'category'];
 
-  constructor(private courseService: CoursesService) {
-    this.courses$ = courseService.list(); // We can use this way, because we put the service in the constructor. We can put this on ngInit as well if you think it's better.
+  constructor(
+    private courseService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = courseService.list()// We can use this way, because we put the service in the constructor. We can put this on ngInit as well if you think it's better.
+    .pipe(
+      catchError(error => {
+        this.onError('Could not load courses');
+        return of([])
+      })
+    ); 
+  }
+
+  onError(errorMessage: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMessage
+    });
   }
 
   ngOnInit(): void {
