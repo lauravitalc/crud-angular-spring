53c85e5414fccc10746755ccde3fe05e6c606861
lauravitalc <laura_vitalc@hotmail.com>
2024-12-04 21:11:24 -0300

    14 - Add spring to java, getting started with backend

---
+++ crud-spring/src/main/java/com/laura/crud_spring/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.laura.crud_spring.controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+}
