fa7a2299112de5a900b3e0edc059e59ece9d83c2
lauravitalc <laura_vitalc@hotmail.com>
2025-01-07 21:42:28 -0300

    21 - POST method for saving the records

--- crud-spring/src/main/java/com/laura/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/laura/crud_spring/controller/CourseController.java
@@ -2,9 +2,10 @@ package com.laura.crud_spring.controller;
 import com.laura.crud_spring.model.Course;
 import com.laura.crud_spring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -21,5 +22,13 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+        //return courseRepository.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 }
