9327e17e0771dcca5942026c4af9758018feb380
lauravitalc <laura_vitalc@hotmail.com>
2025-02-04 20:23:39 -0300

    27 - Create findById on controller

--- crud-spring/src/main/java/com/laura/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/laura/crud_spring/controller/CourseController.java
@@ -24,6 +24,13 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
