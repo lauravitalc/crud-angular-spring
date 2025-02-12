2757415d23bf38e58d0d24a00cd47af26ba54fa2
lauravitalc <laura_vitalc@hotmail.com>
2025-02-11 21:06:52 -0300

    28 and 29 - Enhance CourseForm to support editing; implement save logic for both create and update operations

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -24,7 +24,18 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>){
+    if(record._id){
+      return this.update(record)
+    }
+    return this.create(record)
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record)
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record)
   }
 }
 
